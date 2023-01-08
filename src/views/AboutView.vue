870fed7e7aefec5745f046b7c16b629bd41fffd5
SDen4 <asw325@yandex.ru>
2023-01-08 17:33:16 +0500

    initial

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>About...</h2>
+  </div>
+</template>
+
+<style>
+.about {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/CounterView.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="counter">
+    <h1>This is an counter page</h1>
+    <h2>counter...</h2>
+
+    <div class="btnsWrapper">
+      <button class="btn" @click="count--">Minus</button>
+      <h2>{{ count }}</h2>
+      <button class="btn" @click="count++">Plus</button>
+    </div>
+  </div>
+</template>
+
+<style>
+.counter {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.btnsWrapper {
+  display: flex;
+  margin-top: 10px;
+}
+
+.btn {
+  border: 0;
+  outline: none;
+  border-radius: 5px;
+  margin: 0 20px;
+  cursor: pointer;
+}
+
+@media (min-width: 1024px) {
+  .counter {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
